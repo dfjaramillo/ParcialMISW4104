767529b440d21d0ce98b5d63e3105efb1edf2983
Diego Jaramillo <df.jaramilloa1@uniandes.edu.co>
2024-04-28 16:57:12 -0500

    implement planta service

--- src/app/planta/services/planta.service.ts
+++ src/app/planta/services/planta.service.ts
@@ -1,10 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Planta } from '../models/planta';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantaService {
 
-constructor() { }
+  private apiUrl = environment.baseUrl + '202212_MISW4104_Grupo2.json';
 
+  constructor(private http: HttpClient) { }
+
+  getPlantas(): Observable<Planta[]> {
+    return this.http.get<Planta[]>(this.apiUrl);
+  }
 }
