88a07c4ec568f345eaf4a3b9f18e0ffb68ea4cc5
Diego Jaramillo <df.jaramilloa1@uniandes.edu.co>
2024-04-28 16:59:20 -0500

    update references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlantaModule } from './planta/planta.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderModule } from './components/header/header.module';
+import { FooterModule } from './components/footer/footer/footer.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent        
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PlantaModule,
+    HeaderModule,
+    FooterModule,
+    HttpClientModule,    
   ],
   providers: [],
   bootstrap: [AppComponent]
